0f60303e882cc5e73fa6b36ec7c7f1c04f0655aa
Sudipta <sudiptabala32@gmail.com>
2020-09-27 22:20:52 +0530

    [Sudipta] Add : Welcome Message

---
+++ Welcome.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Welcome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program.");
+        }
+    }
+}
