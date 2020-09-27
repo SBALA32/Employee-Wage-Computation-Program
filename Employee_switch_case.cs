96588c9a1ca0859ded95ddf0240be403ca4b2c85
Sudipta <sudiptabala32@gmail.com>
2020-09-27 23:06:37 +0530

    [Sudipta] Add : This file include check for Full Time or Part Time Employee

---
+++ Employee_switch_case.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace Welcome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int IsFullTime = 1;
+            int IsPartTime = 0;
+            int EmpRatePerHour = 20;
+
+            int empWage = 0;
+            int empHr = 8;
+            empWage = empHr * EmpRatePerHour;
+
+            //Check for Full Time or Part Time
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
+            {
+                case 0:
+                    Console.WriteLine("Part time Employee is present");
+                    Console.WriteLine("Part time Employee Wage = " + empWage);
+                    break;
+
+                case 1:
+                    Console.WriteLine("Full time Employee is present");
+                    Console.WriteLine("Full time Employee Wage = " + empWage);
+                    break;
+
+                default:
+                    Console.WriteLine("Neither Full Time nor Part time Employee is present!");
+                    break;
+            }
+        }
+    }
+}
